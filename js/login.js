2f2099deffd6f69840963fd10f3976c6aacc574a
Chandra_Ivan <124050491+1vannchandra@users.noreply.github.com>
2024-10-22 19:42:28 +0700

    upload tugas4

---
+++ js/login.js
@@ -0,0 +1,9 @@
+const button = document.querySelector('button');
+
+button.addEventListener('click', (event) => {
+    event.preventDefault();
+    const email = document.getElementById('email').value;
+    localStorage.setItem('email', email);
+
+    location.href = '/dashboard.html';
+});
